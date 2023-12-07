89e10f99d8826df2dd216b2cfd39887a37dba314
Doan Manh <doanvanmanh87@gmail.com>
2023-12-07 16:28:08 +0700

    Reduce some lines of code, can improve more

--- SupertrendMTF/SupertrendMTF.cs
+++ SupertrendMTF/SupertrendMTF.cs
@@ -7,19 +7,19 @@ namespace cAlgo.Indicators
     [Indicator(IsOverlay = true, AccessRights = AccessRights.None)]
     public class SupertrendMTF : Indicator
     {
-        [Parameter("Time Frame", DefaultValue = "Daily")]
+        [Parameter("Time Frame", DefaultValue = "m15")]
         public TimeFrame TimeFrame { get; set; }
 
         [Parameter("Period", DefaultValue = 10)]
         public int Period { get; set; }
 
-        [Parameter("Multiplier", DefaultValue = 3.0)]
+        [Parameter("Multiplier", DefaultValue = 7.0)]
         public double Multiplier { get; set; }
 
-        [Output("Up Trend", LineColor = "Green", PlotType = PlotType.Points, Thickness = 3)]
+        [Output("Up Trend", LineColor = "Green", PlotType = PlotType.Points, Thickness = 1)]
         public IndicatorDataSeries UpTrend { get; set; }
 
-        [Output("Down Trend", LineColor = "Red", PlotType = PlotType.Points, Thickness = 3)]
+        [Output("Down Trend", LineColor = "Red", PlotType = PlotType.Points, Thickness = 1)]
         public IndicatorDataSeries DownTrend { get; set; }
 
         private IndicatorDataSeries upBuffer;
@@ -59,46 +59,32 @@ namespace cAlgo.Indicators
             Array.Resize(ref trend, index + 1);
             changeOfTrend = false;
 
-            if (customBars.ClosePrices[customIndex] > upBuffer[index - 1])
-            {
-                trend[index] = 1;
-            }
-            else if (customBars.ClosePrices[customIndex] < downBuffer[index - 1])
-            {
-                trend[index] = -1;
-            }
-            else
-            {
-                trend[index] = trend[index - 1];
-            }
+            trend[index] = customBars.ClosePrices[customIndex] > upBuffer[index - 1] ? 1 :
+                           customBars.ClosePrices[customIndex] < downBuffer[index - 1] ? -1 :
+                           trend[index - 1];
 
             changeOfTrend = trend[index] != trend[index - 1];
 
-            if (trend[index] < 0 && upBuffer[index] > upBuffer[index - 1])
+            if (trend[index] == -1 && upBuffer[index] > upBuffer[index - 1])
             {
                 upBuffer[index] = upBuffer[index - 1];
-            }
-
-            if (trend[index] > 0 && downBuffer[index] < downBuffer[index - 1])
-            {
-                downBuffer[index] = downBuffer[index - 1];
-            }
-
-            if (trend[index] == 1)
-            {
-                UpTrend[index] = downBuffer[index];
-                if (changeOfTrend)
-                {
-                    UpTrend[index - 1] = DownTrend[index - 1];
-                    changeOfTrend = false;
-                }
-            }
-            else if (trend[index] == -1)
-            {
+                
                 DownTrend[index] = upBuffer[index];
                 if (changeOfTrend)
                 {
                     DownTrend[index - 1] = UpTrend[index - 1];
+                    changeOfTrend = false;
+                }               
+            }
+
+            if (trend[index] == 1 && downBuffer[index] < downBuffer[index - 1])
+            {
+                downBuffer[index] = downBuffer[index - 1];
+                
+                UpTrend[index] = downBuffer[index];
+                if (changeOfTrend)
+                {
+                    UpTrend[index - 1] = DownTrend[index - 1];
                     changeOfTrend = false;
                 }
             }
