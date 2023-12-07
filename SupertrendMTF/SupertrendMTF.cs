04c0043fc5754baac0c80e4e562cb09d5abd5f02
Doan Manh <doanvanmanh87@gmail.com>
2023-12-07 13:14:52 +0700

    Update name styple

--- SupertrendMTF/SupertrendMTF.cs
+++ SupertrendMTF/SupertrendMTF.cs
@@ -22,113 +22,86 @@ namespace cAlgo.Indicators
         [Output("Down Trend", LineColor = "Red", PlotType = PlotType.Points, Thickness = 3)]
         public IndicatorDataSeries DownTrend { get; set; }
 
-        private IndicatorDataSeries _upBuffer;
-        private IndicatorDataSeries _downBuffer;
-        private AverageTrueRange _averageTrueRange;
-        private int[] _trend;
-        private bool _changeOfTrend;
-        private Bars _customBars;
+        private IndicatorDataSeries upBuffer;
+        private IndicatorDataSeries downBuffer;
+        private AverageTrueRange averageTrueRange;
+        private int[] trend;
+        private bool changeOfTrend;
+        private Bars customBars;
 
         protected override void Initialize()
         {
-            _customBars = MarketData.GetBars(TimeFrame);
-            _trend = new int[1];
-            _upBuffer = CreateDataSeries();
-            _downBuffer = CreateDataSeries();
-            _averageTrueRange = Indicators.AverageTrueRange(_customBars, Period, MovingAverageType.Simple);
+            customBars = MarketData.GetBars(TimeFrame);
+            trend = new int[1];
+            upBuffer = CreateDataSeries();
+            downBuffer = CreateDataSeries();
+            averageTrueRange = Indicators.AverageTrueRange(customBars, Period, MovingAverageType.Simple);
         }
 
         public override void Calculate(int index)
         {
+            UpTrend[index] = double.NaN;
+            DownTrend[index] = double.NaN;
+
             if (index < 1)
             {
-                InitializeTrend(index);
+                trend[index] = 1;
                 return;
             }
 
-            UpdateTrend(index);
+            int customIndex = customBars.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
+            double median = (customBars.HighPrices[customIndex] + customBars.LowPrices[customIndex]) / 2;
+            double atr = averageTrueRange.Result[customIndex];
 
-            if (_trend[index] == 1)
+            upBuffer[index] = median + Multiplier * atr;
+            downBuffer[index] = median - Multiplier * atr;
+
+            Array.Resize(ref trend, index + 1);
+            changeOfTrend = false;
+
+            if (customBars.ClosePrices[customIndex] > upBuffer[index - 1])
             {
-                UpTrend[index] = _downBuffer[index];
-                HandleChangeOfTrend(index, isUpTrend: true);
+                trend[index] = 1;
             }
-            else if (_trend[index] == -1)
+            else if (customBars.ClosePrices[customIndex] < downBuffer[index - 1])
             {
-                DownTrend[index] = _upBuffer[index];
-                HandleChangeOfTrend(index, isUpTrend: false);
-            }
-        }
-
-        private void InitializeTrend(int index)
-        {
-            UpTrend[index] = double.NaN;
-            DownTrend[index] = double.NaN;
-            _trend[index] = 1;
-        }
-
-        private void UpdateTrend(int index)
-        {
-            int customIndex = _customBars.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
-            double median = (_customBars.HighPrices[customIndex] + _customBars.LowPrices[customIndex]) / 2;
-            double atr = _averageTrueRange.Result[customIndex];
-
-            _upBuffer[index] = median + Multiplier * atr;
-            _downBuffer[index] = median - Multiplier * atr;
-            _changeOfTrend = false;
-
-            UpdateTrendDirection(index, customIndex);
-
-            AdjustBuffersForTrendContinuation(index);
-        }
-
-        private void UpdateTrendDirection(int index, int customIndex)
-        {
-            Array.Resize(ref _trend, index + 1);
-
-            if (_customBars.ClosePrices[customIndex] > _upBuffer[index - 1])
-            {
-                _trend[index] = 1;
-            }
-            else if (_customBars.ClosePrices[customIndex] < _downBuffer[index - 1])
-            {
-                _trend[index] = -1;
+                trend[index] = -1;
             }
             else
             {
-                _trend[index] = _trend[index - 1];
+                trend[index] = trend[index - 1];
             }
 
-            _changeOfTrend = _trend[index] != _trend[index - 1];
-        }
+            changeOfTrend = trend[index] != trend[index - 1];
 
-        private void AdjustBuffersForTrendContinuation(int index)
-        {
-            if (_trend[index] < 0 && _upBuffer[index] > _upBuffer[index - 1])
+            if (trend[index] < 0 && upBuffer[index] > upBuffer[index - 1])
             {
-                _upBuffer[index] = _upBuffer[index - 1];
+                upBuffer[index] = upBuffer[index - 1];
             }
 
-            if (_trend[index] > 0 && _downBuffer[index] < _downBuffer[index - 1])
+            if (trend[index] > 0 && downBuffer[index] < downBuffer[index - 1])
             {
-                _downBuffer[index] = _downBuffer[index - 1];
+                downBuffer[index] = downBuffer[index - 1];
             }
-        }
 
-        private void HandleChangeOfTrend(int index, bool isUpTrend)
-        {
-            if (!_changeOfTrend) return;
-
-            if (isUpTrend)
+            if (trend[index] == 1)
             {
-                UpTrend[index - 1] = DownTrend[index - 1];
+                UpTrend[index] = downBuffer[index];
+                if (changeOfTrend)
+                {
+                    UpTrend[index - 1] = DownTrend[index - 1];
+                    changeOfTrend = false;
+                }
             }
-            else
+            else if (trend[index] == -1)
             {
-                DownTrend[index - 1] = UpTrend[index - 1];
+                DownTrend[index] = upBuffer[index];
+                if (changeOfTrend)
+                {
+                    DownTrend[index - 1] = UpTrend[index - 1];
+                    changeOfTrend = false;
+                }
             }
-
-            _changeOfTrend = false;
         }
     }
 }
